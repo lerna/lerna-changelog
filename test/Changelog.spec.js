b4da8ebb1304945077594d0f96a5638ac232d640
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-27 04:51:45 +0200

    Update "jest" to v20.0.4 (#50)
    * Update "jest" to v20.0.4
    
    * Update "babel-jest" to v20.0.3
    
    * test: Move setup code into tests or beforeEach() blocks

--- test/Changelog.spec.js
+++ test/Changelog.spec.js
@@ -6,20 +6,23 @@ jest.mock("../src/execSync");
 
 describe("contructor", () => {
   const MockedChangelog = require("../src/Changelog").default;
-  const testConfig = require("../src/Changelog").__getConfig();
 
   beforeEach(() => {
     require("../src/Changelog").__resetDefaults();
   });
 
   it("set config", () => {
+    const testConfig = require("../src/Changelog").__getConfig();
+
     const changelog = new MockedChangelog();
     expect(changelog.config).toEqual(testConfig);
   });
+
   it("set remote", () => {
     const changelog = new MockedChangelog();
     expect(changelog.remote).toBeDefined();
   });
+
   it("set cli options", () => {
     const changelog = new MockedChangelog({ "tag-from": "1", "tag-to": "2" });
     expect(changelog.tagFrom).toBe("1");
@@ -31,48 +34,50 @@ describe("getCommitsInfo", () => {
   beforeEach(() => {
     require("../src/execSync").__resetDefaults();
     require("../src/ApiDataCache").__resetDefaults();
+
+    require("../src/execSync").__mockGitLog(
+      "a0000005;HEAD -> master, tag: v0.2.0, origin/master, " +
+      "origin/HEAD;chore(release): releasing component;2017-01-01\n" +
+      "a0000004;;Merge pull request #2 from my-feature;2017-01-01\n" +
+      "a0000003;;feat(module) Add new module (#2);2017-01-01\n" +
+      "a0000002;;refactor(module) Simplify implementation;2017-01-01\n" +
+      "a0000001;tag: v0.1.0;chore(release): releasing component;2017-01-01"
+    );
+    require("../src/execSync").__mockGitTag(
+      "v0.2.0\n" +
+      "v0.1.1\n" +
+      "v0.1.0\n" +
+      "v0.0.1"
+    );
+    const usersCache = {
+      "test-user": {
+        login: "test-user",
+        html_url: "https://github.com/test-user",
+        name: "Test User"
+      },
+    };
+    const issuesCache = {
+      2: {
+        number: 2,
+        title: "This is the commit title for the issue (#2)",
+        labels: [
+          { name: "Type: New Feature" },
+          { name: "Status: In Progress" },
+        ],
+        user: usersCache["test-user"],
+      }
+    };
+    require("../src/ApiDataCache").__setCache({
+      user: usersCache,
+      issue: issuesCache,
+    });
   });
-  require("../src/execSync").__mockGitLog(
-    "a0000005;HEAD -> master, tag: v0.2.0, origin/master, " +
-    "origin/HEAD;chore(release): releasing component;2017-01-01\n" +
-    "a0000004;;Merge pull request #2 from my-feature;2017-01-01\n" +
-    "a0000003;;feat(module) Add new module (#2);2017-01-01\n" +
-    "a0000002;;refactor(module) Simplify implementation;2017-01-01\n" +
-    "a0000001;tag: v0.1.0;chore(release): releasing component;2017-01-01"
-  );
-  require("../src/execSync").__mockGitTag(
-    "v0.2.0\n" +
-    "v0.1.1\n" +
-    "v0.1.0\n" +
-    "v0.0.1"
-  );
-  const usersCache = {
-    "test-user": {
-      login: "test-user",
-      html_url: "https://github.com/test-user",
-      name: "Test User"
-    },
-  };
-  const issuesCache = {
-    2: {
-      number: 2,
-      title: "This is the commit title for the issue (#2)",
-      labels: [
-        { name: "Type: New Feature" },
-        { name: "Status: In Progress" },
-      ],
-      user: usersCache["test-user"],
-    }
-  };
-  require("../src/ApiDataCache").__setCache({
-    user: usersCache,
-    issue: issuesCache,
-  });
-  const MockedChangelog = require("../src/Changelog").default;
-  const changelog = new MockedChangelog();
-  const commitsInfo = changelog.getCommitsInfo();
 
   it("parse commits with different tags", () => {
+    const MockedChangelog = require("../src/Changelog").default;
+    const changelog = new MockedChangelog();
+    const commitsInfo = changelog.getCommitsInfo();
+
     expect(commitsInfo).toEqual([
       {
         commitSHA: "a0000005",
@@ -136,24 +141,24 @@ describe("getCommitsInfo", () => {
 });
 
 describe("getCommitsByCategory", () => {
-  const MockedChangelog = require("../src/Changelog").default;
-  const changelog = new MockedChangelog();
-  const testCommits = [
-    { commitSHA: "a0000005", labels: [{ name: "Status: In Progress" }] },
-    { commitSHA: "a0000004", labels: [{ name: "Type: Bug" }] },
-    {
-      commitSHA: "a0000003",
-      labels: [
-        { name: "Type: New Feature" },
-        { name: "Status: In Progress" },
-      ]
-    },
-    { commitSHA: "a0000002", labels: [] },
-    { commitSHA: "a0000001", labels: [] }
-  ];
-  const commitsByCategory = changelog.getCommitsByCategory(testCommits);
-
   it("group commits by category", () => {
+    const MockedChangelog = require("../src/Changelog").default;
+    const changelog = new MockedChangelog();
+    const testCommits = [
+      { commitSHA: "a0000005", labels: [{ name: "Status: In Progress" }] },
+      { commitSHA: "a0000004", labels: [{ name: "Type: Bug" }] },
+      {
+        commitSHA: "a0000003",
+        labels: [
+          { name: "Type: New Feature" },
+          { name: "Status: In Progress" },
+        ]
+      },
+      { commitSHA: "a0000002", labels: [] },
+      { commitSHA: "a0000001", labels: [] }
+    ];
+    const commitsByCategory = changelog.getCommitsByCategory(testCommits);
+
     expect(commitsByCategory).toEqual([
       {
         commits: [
@@ -184,47 +189,48 @@ describe("getCommitsByCategory", () => {
 describe("getCommitters", () => {
   beforeEach(() => {
     require("../src/ApiDataCache").__resetDefaults();
-  });
 
-  const usersCache = {
-    "test-user": {
-      login: "test-user",
-      html_url: "https://github.com/test-user",
-      name: "Test User"
-    },
-    "test-user-1": {
-      login: "test-user-1",
-      html_url: "https://github.com/test-user-1",
-      name: "Test User 1"
-    },
-    "test-user-2": {
-      login: "test-user-2",
-      html_url: "https://github.com/test-user-2",
-      name: "Test User 2"
-    },
-    "user-bot": {
-      login: "user-bot",
-      html_url: "https://github.com/user-bot",
-      name: "User Bot"
-    },
-  };
-  require("../src/ApiDataCache").__setCache({
-    user: usersCache,
-    issue: {},
+    const usersCache = {
+      "test-user": {
+        login: "test-user",
+        html_url: "https://github.com/test-user",
+        name: "Test User"
+      },
+      "test-user-1": {
+        login: "test-user-1",
+        html_url: "https://github.com/test-user-1",
+        name: "Test User 1"
+      },
+      "test-user-2": {
+        login: "test-user-2",
+        html_url: "https://github.com/test-user-2",
+        name: "Test User 2"
+      },
+      "user-bot": {
+        login: "user-bot",
+        html_url: "https://github.com/user-bot",
+        name: "User Bot"
+      },
+    };
+    require("../src/ApiDataCache").__setCache({
+      user: usersCache,
+      issue: {},
+    });
+    require("../src/Changelog").__setConfig({ ignoreCommitters: ["user-bot"] });
   });
-  require("../src/Changelog").__setConfig({ ignoreCommitters: ["user-bot"] });
-  const MockedChangelog = require("../src/Changelog").default;
-  const changelog = new MockedChangelog();
-
-  const testCommits = [
-    { commitSHA: "a0000004", user: { login: "test-user-1" } },
-    { commitSHA: "a0000003", user: { login: "test-user-2" } },
-    { commitSHA: "a0000002", user: { login: "user-bot" } },
-    { commitSHA: "a0000001" },
-  ];
-  const committers = changelog.getCommitters(testCommits);
 
   it("get list of valid commiters", () => {
+    const MockedChangelog = require("../src/Changelog").default;
+    const changelog = new MockedChangelog();
+
+    const testCommits = [
+      { commitSHA: "a0000004", user: { login: "test-user-1" } },
+      { commitSHA: "a0000003", user: { login: "test-user-2" } },
+      { commitSHA: "a0000002", user: { login: "user-bot" } },
+      { commitSHA: "a0000001" },
+    ];
+    const committers = changelog.getCommitters(testCommits);
+
     expect(committers).toEqual([
       "Test User 1 ([test-user-1](https://github.com/test-user-1))",
       "Test User 2 ([test-user-2](https://github.com/test-user-2))"
