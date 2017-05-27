7730fc44b66467b0c9b1b666f67dc048d1b514ec
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-27 17:31:15 +0200

    ConfigurationError: Convert to class

--- src/ConfigurationError.js
+++ src/ConfigurationError.js
@@ -1,13 +1,6 @@
-// Can't use `instanceof` with babel-ified subclasses of builtins.
-//
-//   https://phabricator.babeljs.io/T3083
-//
-// Gotta do this the old-fashioned way. :p
-//
-export default function ConfigurationError(message) {
-  this.name = "ConfigurationError";
-  this.message = message;
-  this.stack = (new Error()).stack;
+export default class ConfigurationError {
+  constructor(message) {
+    Error.apply(this, arguments);
+    this.name = "ConfigurationError";
+  }
 }
-ConfigurationError.prototype = Object.create(Error.prototype);
-ConfigurationError.prototype.constructor = ConfigurationError;
