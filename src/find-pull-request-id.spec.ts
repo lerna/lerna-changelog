38feeb1b3742701eac9457aa705a3fcaf73ba499
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-28 13:35:14 +0200

    find-pull-request-id: Add support for multi-line commit messages

--- src/find-pull-request-id.spec.ts
+++ src/find-pull-request-id.spec.ts
@@ -8,7 +8,7 @@ describe("findPullRequestId", function() {
   });
 
   it("finds the id in a GitHub squash-merge commit", function() {
-    const message = "Adjust \"lint\" script (#48)";
+    const message = "Adjust \"lint\" script (#48)\n\n* bin/cli: Use \"const\" instead of \"var\"\n\n* package.json: Adjust \"lint\" script";
     const result = findPullRequestId(message);
     expect(result).toEqual("48");
   });
--- src/find-pull-request-id.ts
+++ src/find-pull-request-id.ts
@@ -1,11 +1,14 @@
 export default function findPullRequestId(message: string): string | null {
+  const lines = message.split("\n");
+  const firstLine = lines[0];
+
   if (message.indexOf("Merge pull request ") === 0) {
     const start = message.indexOf("#") + 1;
     const end = message.slice(start).indexOf(" ");
     return message.slice(start, start + end);
   }
 
-  const mergeCommit = message.match(/\(#(\d+)\)$/);
+  const mergeCommit = firstLine.match(/\(#(\d+)\)$/);
   if (mergeCommit) {
     return mergeCommit[1];
   }
