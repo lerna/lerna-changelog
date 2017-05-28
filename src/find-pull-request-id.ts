e47480dd220c05c695c129b662ea33ca678abae2
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-28 13:39:37 +0200

    find-pull-request-id: Use regex to extract PR id

--- src/find-pull-request-id.ts
+++ src/find-pull-request-id.ts
@@ -2,15 +2,14 @@ export default function findPullRequestId(message: string): string | null {
   const lines = message.split("\n");
   const firstLine = lines[0];
 
-  if (message.indexOf("Merge pull request ") === 0) {
-    const start = message.indexOf("#") + 1;
-    const end = message.slice(start).indexOf(" ");
-    return message.slice(start, start + end);
+  const mergeMatch = firstLine.match(/^Merge pull request #(\d+) from /);
+  if (mergeMatch) {
+    return mergeMatch[1];
   }
 
-  const mergeCommit = firstLine.match(/\(#(\d+)\)$/);
-  if (mergeCommit) {
-    return mergeCommit[1];
+  const squashMergeMatch = firstLine.match(/\(#(\d+)\)$/);
+  if (squashMergeMatch) {
+    return squashMergeMatch[1];
   }
 
   const homuMatch = firstLine.match(/^Auto merge of #(\d+) - /);
