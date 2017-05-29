8a9afd545dc432dfc31f4e93de76b27bb5494541
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-29 17:20:16 +0200

    Remove unused execSync() function

--- src/exec-sync.ts
+++
@@ -1,7 +0,0 @@
-const child = require("child_process");
-
-export default function execSync(cmd: string, options = {}) {
-  const opts = { encoding: "utf8", ...options };
-
-  return child.execSync(cmd, opts).trim();
-}
