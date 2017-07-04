794695ffae9bef7d4b0adc70613803bd71a25a08
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-04 22:46:53 +0200

    configuration: Derive repo from package.json and use default labels

--- src/configuration.spec.ts
+++ src/configuration.spec.ts
@@ -2,7 +2,7 @@ const os = require("os");
 const fs = require("fs-extra");
 const path = require("path");
 
-import { fromGitRoot, fromPath } from "./configuration";
+import { fromGitRoot, fromPath, findRepoFromPkg } from "./configuration";
 import ConfigurationError from "./configuration-error";
 
 describe("Configuration", function() {
@@ -70,6 +70,35 @@ describe("Configuration", function() {
 
     it("throws ConfigurationError if neither 'package.json' nor 'lerna.json' exist", function() {
       expect(() => fromPath(tmpDir)).toThrowError(ConfigurationError);
+    });
+  });
+
+  describe("findRepoFromPkg", function() {
+    const tests = [
+      ['git+https://github.com/ember-cli/ember-rfc176-data.git', 'ember-cli/ember-rfc176-data'],
+      ['https://github.com/ember-cli/ember-rfc176-data.git', 'ember-cli/ember-rfc176-data'],
+      ['https://github.com/babel/ember-cli-babel', 'babel/ember-cli-babel'],
+      ['https://github.com/babel/ember-cli-babel.git', 'babel/ember-cli-babel'],
+      ['git@github.com:babel/ember-cli-babel.git', 'babel/ember-cli-babel'],
+      ['https://gitlab.com/gnachman/iterm2.git', undefined],
+      ['git@gitlab.com:gnachman/iterm2.git', undefined],
+    ];
+
+    tests.forEach(([input, output]) => {
+      it(`'${input}' -> '${output}'`, function() {
+        expect(findRepoFromPkg({
+          repository: {
+            type: 'git',
+            url: input,
+          },
+        })).toEqual(output);
+      });
+    });
+
+    it(`works with shorthand 'repository' syntax`, function() {
+      expect(findRepoFromPkg({
+        repository: 'https://github.com/babel/ember-cli-babel',
+      })).toEqual('babel/ember-cli-babel');
     });
   });
 });
